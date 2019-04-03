cd386b96391108d99f4eb51585a17e4f9f8778a7
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-04-03 16:50:22 +0900

    TextField test

--- TestTextField01/ViewController.swift
+++ TestTextField01/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myTextField.clearButtonMode = UITextField.ViewMode.always
+        myTextField.delegate = self
+    }
+    @IBAction func bt(_ sender: Any) {
+        
+        let inString = myTextField.text! + "Coding"
+        myLabel.text = inString
+        myTextField.text = ""
+        
+        myTextField.resignFirstResponder() //키패드의 권한을 박탈
+        
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+        
+    }
+    
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        myTextField.backgroundColor = UIColor.yellow
+        return true
+        
     }
 
-
 }
-
